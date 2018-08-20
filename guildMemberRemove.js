e0928a0ff5e073b8baeb9b21a927465677130c5a
merickocakgithub <42494366+merickocakgithub@users.noreply.github.com>
2018-08-20 16:40:32 +0300

    Delete guildMemberRemove.js

--- guildMemberRemove.js
+++
@@ -1,5 +0,0 @@
-module.exports = member => {
-  const channel = member.guild.channels.find('name', 'giris-cikis');
-  if (!channel) return;
-  channel.send(`**📤 |** ${member} **Sunucudan çıkış yaptı!**`);
-};
